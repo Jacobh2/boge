046a6daae8a0daa53ebdfade3d7d5414fd29c27d
Jacob Hagstedt <jacob.hagstedt@gmail.com>
2023-08-29 22:55:58 +0200

    Fixes to make it work on rpi

--- app/sensor/humidity.py
+++ app/sensor/humidity.py
@@ -1,12 +1,11 @@
 
-
 class Humidity:
-    DHT_PIN = 4
     ERROR_IN_ROW_LIMIT = 10
 
     def __init__(self):
         import adafruit_dht
-        self.device = adafruit_dht.DHT22(self.DHT_PIN)
+        import board
+        self.device = adafruit_dht.DHT22(board.D4)
         self.prev_humidity: float = -999
         self.prev_temperature: float = -999
         self.errors_in_row = 0
