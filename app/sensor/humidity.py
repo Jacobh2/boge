f56eb00f571ee67fe68ca5991050e92f7ff9e871
Jacob Hagstedt <jacob.hagstedt@gmail.com>
2024-03-17 20:10:42 +0100

    Handle first fail

--- app/sensor/humidity.py
+++ app/sensor/humidity.py
@@ -15,17 +15,21 @@ class Humidity(Sensor):
         import board
 
         self.device = adafruit_dht.DHT22(board.D4)
-        self.prev_humidity: float = -999
-        self.prev_temperature: float = -999
+        self.prev_humidity = None
+        self.prev_temperature = None
         self.errors_in_row_temperature = 0
         self.errors_in_row_humidity = 0
+        self.last_temperature_read_ok = False
+        self.last_humidity_read_ok = False
 
     def try_get_temperature(self):
         try:
             self.prev_temperature = self.device.temperature
             self.errors_in_row_temperature = 0
+            self.last_temperature_read_ok = True
         except RuntimeError:
             self.errors_in_row_temperature += 1
+            self.last_temperature_read_ok = False
             if self.errors_in_row_temperature > self.ERROR_IN_ROW_LIMIT:
                 logger.warning(
                     "Failed to read temperature after %s",
@@ -38,8 +42,10 @@ class Humidity(Sensor):
         try:
             self.prev_humidity = self.device.humidity
             self.errors_in_row_humidity = 0
+            self.last_humidity_read_ok = True
         except RuntimeError:
             self.errors_in_row_humidity += 1
+            self.last_humidity_read_ok = False
             if self.errors_in_row_humidity > self.ERROR_IN_ROW_LIMIT:
                 logger.warning(
                     "Failed to read humidity after %s",
@@ -62,9 +68,17 @@ class Humidity(Sensor):
         return self.prev_humidity
 
     def get_temperature_status(self) -> bool:
-        logger.info("Temperature status: Errors=%s and is alive=%s", self.errors_in_row_temperature, self.is_alive())
-        return self.errors_in_row_temperature == 0 and self.is_alive()
+        logger.info(
+            "Temperature status: Errors=%s and is alive=%s",
+            self.errors_in_row_temperature,
+            self.is_alive(),
+        )
+        return self.last_temperature_read_ok == 0 and self.is_alive()
 
     def get_humidity_status(self) -> bool:
-        logger.info("Humidity status: Errors=%s and is alive=%s", self.errors_in_row_humidity, self.is_alive())
-        return self.errors_in_row_humidity == 0 and self.is_alive()
+        logger.info(
+            "Humidity status: Errors=%s and is alive=%s",
+            self.errors_in_row_humidity,
+            self.is_alive(),
+        )
+        return self.last_humidity_read_ok == 0 and self.is_alive()
